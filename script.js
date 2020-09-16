6e5b093810737363e57c7c64089b682f1a0390a2
Joco Bogdanovic <b.joco@outlook.com>
2020-09-16 19:52:24 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var bg = document.getElementById("gradient");
+function setGradient() {
+  bg.style.background =
+    "linear-gradient(to left, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = bg.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
